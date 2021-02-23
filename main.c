16ece4b361334f3fc5cee6efc9ce9bcf76b79220
envzhu <envzhu@gmail.com>
2021-02-23 20:24:02 +0900

    Fix CMakeLists.txt

--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 #include <bfd.h>
 
 int main (int argc, char *argv[]) {
+  bfd_init();
+  bfd *abfd = bfd_openr(fname, "elf64-x86-64");
   FILE *input_fp = fopen(argv[1], 'r');
   FILE *output_fp = fopen(argv[2], 'w');
   if ( input_fp == NULL || output_fp == NULL)
     return -1;
-  
 }
